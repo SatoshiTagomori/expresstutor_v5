cde810b6e4bc25e0caea545b6fdb01803dbd0c94
田子森聰 <satoshi@tagomorisounoMac-mini.local>
2021-02-19 21:41:15 +0900

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,7 @@ class StudentsController < ApplicationController
     require 'json'
 
     @res = get_access_token(params[:code])
-    @res2 = get_line_user_info(@res.access_token)
+    @res3 = JSON.parse(@res.body)
+    @res2 = get_line_user_info(@res3["access_token"])
   end
 end
