1c31596412f5ea1cbc270e14528534e65b8fb941
田子森聰 <satoshi@tagomorisounoMac-mini.local>
2021-02-19 21:33:09 +0900

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,7 @@ class StudentsController < ApplicationController
     require 'uri'
     require 'json'
 
-    @res = get_access_token
+    @res = get_access_token(params[:code])
+    @res2 = get_line_user_info(@res.access_token)
   end
 end
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,5 +1,5 @@
 module StudentsHelper
-    def get_access_token
+    def get_access_token(code)
         uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
@@ -7,7 +7,20 @@ module StudentsHelper
         grant_type = "authorization_code"
         http.start do
           req = Net::HTTP::Post.new(uri.path)
-          req.set_form_data(grant_type: grant_type,code: params[:code],redirect_uri: 'https://expresstutor.tokyo/students',client_id: LINE_CLIENT_ID,client_secret: '9bf7eeb456791ccd36b8c775531f9ecf')
+          req.set_form_data(grant_type: grant_type,code: code,redirect_uri: LINE_REDIRECT_URL,client_id: LINE_CLIENT_ID,client_secret: LINE_SECRET)
+          return http.request(req)
+        end    
+    end
+
+    def get_line_user_info(access_token)
+        uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        grant_type = "authorization_code"
+        http.start do
+          req = Net::HTTP::Post.new(uri.path)
+          req.set_form_data(id_token: access_token,client_id: LINE_CLIENT_ID)
           return http.request(req)
         end    
     end
