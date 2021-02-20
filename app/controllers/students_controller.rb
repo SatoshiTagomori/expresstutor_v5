e17b4ac6accac082e4708a22d13fe8565867185a
田子森聰 <satoshi@tagomorisounoMac-mini.local>
2021-02-20 23:40:03 +0900

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,10 +12,10 @@ class StudentsController < ApplicationController
     if is_no_error()
       #ユーザー情報を取得する
       @user_info = get_user_info(access_token)
-      student = Student.find_by(:lineid => @user_info.userId)
-      if student.blank?
-        Student.create(:name=>@user_info.displayName,:lineid=>@user_info.userId)
-      end
+      #student = Student.find_by(:lineid => @user_info.userId)
+      #if student.blank?
+      #  Student.create(:name=>@user_info.displayName,:lineid=>@user_info.userId)
+      #end
     end
   end
 end
