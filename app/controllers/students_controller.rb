8c9005d09efa01e7b9ea60d87dc9bf57faa402e6
田子森聰 <satoshi@tagomorisounoMac-mini.local>
2021-02-21 19:35:56 +0900

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,17 +4,29 @@ class StudentsController < ApplicationController
     require 'uri'
     require 'json'
     require 'base64'
-
-    #アクセストークンを取得してセッションにいれる
-    access_token = get_access_token(params[:code])
-    #エラーがなければ
-    if is_no_error()
-      #ユーザー情報を取得する
-      @user_info = get_user_info(access_token)
-      student = Student.find_by(:lineid => @user_info["userId"])
-      if student.blank?
-        Student.create(:name=>@user_info["displayName"],:lineid=>@user_info["userId"])
+    #アクセストークンが有効であれば
+    if check_access_token
+      @user_info= get_user_info(access_token)
+      @student = Student.find_by(:lineid => @user_info["userId"])
+      if @student.blank?
+        @student = Student.create(:name=>@user_info["displayName"],:lineid=>@user_info["userId"])
+      end
+    else
+      #アクセストークンを取得してセッションにいれる
+      access_token = get_access_token(params[:code])
+      #エラーがなければ
+      if is_no_error()
+        #ユーザー情報を取得する
+        @user_info = get_user_info(access_token)
+        @student = Student.find_by(:lineid => @user_info["userId"])
+        if @student.blank?
+          @student = Student.create(:name=>@user_info["displayName"],:lineid=>@user_info["userId"])
+        end
       end
     end
+    
   end
+
+
+
 end
