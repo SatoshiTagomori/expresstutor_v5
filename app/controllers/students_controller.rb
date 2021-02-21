7fd1b073b3a98f7ebd11dd8b0d8e070c5023c822
田子森聰 <satoshi@tagomorisounoMac-mini.local>
2021-02-21 10:57:33 +0900

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,15 +7,14 @@ class StudentsController < ApplicationController
 
     #アクセストークンを取得してセッションにいれる
     access_token = get_access_token(params[:code])
-    @a_token = access_token
     #エラーがなければ
     if is_no_error()
       #ユーザー情報を取得する
       @user_info = get_user_info(access_token)
-      #student = Student.find_by(:lineid => @user_info.userId)
-      #if student.blank?
-      #  Student.create(:name=>@user_info.displayName,:lineid=>@user_info.userId)
-      #end
+      student = Student.find_by(:lineid => @user_info["userId"])
+      if student.blank?
+        Student.create(:name=>@user_info["displayName"],:lineid=>@user_info["userId"])
+      end
     end
   end
 end
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -38,6 +38,7 @@ module StudentsHelper
 
     def get_access_token(code)
         access_token_response = get_access_token_response(code)
+        logger.debug access_token_response
         if access_token_response.code != '200'
             set_error(access_token_response.to_s)
             set_error('アクセストークンの取得に失敗しました。')
